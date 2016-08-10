5e17dc382a85ddbdd6c46484e0d9e02e102da8cd
ozidna <zidnaothmane@gmail.com>
2016-08-10 15:59:18 +0200

    should exist the generator.js

---
+++ spec/test.spec.js
@@ -0,0 +1,15 @@
+var Generetor = require('../lib/generetorNumbre');
+'use strict'
+
+var expect = require('chai').expect;
+describe("generator the numbers from 1 to 100", function() {
+
+    it("should exist", function () {
+
+        expect(Generetor).to.not.be.undefined;
+    });
+
+    
+
+
+})
