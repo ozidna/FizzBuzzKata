e9056cdf460743097bd39bd830ed15fda7773eff
ozidna <zidnaothmane@gmail.com>
2016-08-10 16:42:54 +0200

    second test when the inut is devisible by 3

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -8,11 +8,18 @@ describe("generator the numbers from 1 to 100", function() {
 
         expect(Generetor).to.not.be.undefined;
     });
-    it("shoulde returne 1 when the function start the 1 to 1", function () {
+    it("shoulde returne 1 when the function start the 1", function () {
         var input=1;
         var expected =1;
         var actual= Generetor.firstGenaration(input);
         expect(actual).to.eql(expected);
     });
+
+    it("shoulde returne Fizz when the inpute is 3",function () {
+        var input=3;
+        var expected ="Fizz";
+        var actual= Generetor.firstGenaration(input);
+        expect(actual).to.eql(expected);
+    } )
 
 });
