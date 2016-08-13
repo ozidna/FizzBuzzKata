41e7adbb9a2e9ea7583633ccfa8389a72b9466be
ozidna <zidnaothmane@gmail.com>
2016-08-13 20:44:39 +0200

    end VERSION3

--- main.js
+++ main.js
@@ -3,18 +3,21 @@
  */
 var checkerFizzBuzz = require('./lib/generetorNumbre');
 var scanf = require('scanf');
-var firstAndSecondMultiple=[3,5];
+var firstAndSecondMultiple = [3, 5];
 
 console.log("\n\n*********************** Bonjour ***********************\n\n")
-console.log("Menu:\n");
+console.log("***********************Menu***********************:\n");
 console.log("1- executer le program FizzBuzzKata                    0- Quitter \n");
 console.log("Merci de saisir votre choix?  \n");
 var number = scanf('%d');
-
-while (number !== 0) {
-    console.log("\nMerci de saisir un nombre entre 1 et 100 si voux t'apper 0 vous allez quitter l'application?  \n");
+while ((number !== 0)) {
+    console.log("\n!!!!!N.B!!!!! :\n si vous avez pas remplir les deux multiple de fizz et Buzz la valeur par defaut et 3,5\n" +
+        "le generator peut fonctioner par un seul multiple\n");
+    console.log("\nMerci de saisir le multiple de Fizz,?  \n");
+    var multipleFizz = scanf('%d');
+    console.log("\nMerci de saisir le multiple de Buzz?  \n");
+    var multipleBuzz = scanf('%d');
+    console.log(checkerFizzBuzz.Generetor([multipleFizz, multipleBuzz]) + "\n");
+    console.log("\nvoulez vous continuer a executer le programe si oui taper 1 si non tapper 0  \n");
     var number = scanf('%d');
-    if (number !== 0 &&(number!==null)) {
-        console.log(checkerFizzBuzz.firstGenaration(number,firstAndSecondMultiple) + "\n");
-    }
 }
